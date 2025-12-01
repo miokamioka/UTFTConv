c65e3199dd0d026d43f34cb2bc4a5abbb7b926ae
kam1o <arufa194s@gmail.com>
2025-12-01 14:01:07 +0900

    プロジェクトファイルを追加。

---
+++ UTFTConv/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace UTFTConv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(321, 313);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(43, 15);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "label1";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(146, 343);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(430, 343);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(114, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 202);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ UTFTConv/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTFTConv
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _currentBitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "画像ファイル(*.bmp;*.jpg;*.png;*.jpeg)|*.bmp;*.jpg;*.png;*.jpeg|すべてのファイル(*.*)|*.*";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        _currentBitmap = new Bitmap(fs);
+                    }
+                    pictureBox1.Image = _currentBitmap;
+                    lblStatus.Text = $"Loaded: {_currentBitmap.Width}x{_currentBitmap.Height}";
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_currentBitmap == null) return;
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "C Source File|*.c";
+                sfd.FileName = "image_data.c";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    string arrayName = Path.GetFileNameWithoutExtension(sfd.FileName);
+                    string cContent = GenerateCFileContent(_currentBitmap, arrayName);
+
+                    File.WriteAllText(sfd.FileName, cContent);
+                    lblStatus.Text = $"Saved: {sfd.FileName}";
+                    MessageBox.Show("done.");
+                }
+            }
+        }
+        private string GenerateCFileContent(Bitmap bmp, string arrayName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // hedder
+            sb.AppendLine($"// Image Size: {bmp.Width}x{bmp.Height}");
+            sb.AppendLine("#include <avr/pgmspace.h>");
+            sb.AppendLine();
+            sb.AppendLine($"const unsigned short {arrayName}[{bmp.Width * bmp.Height}] PROGMEM = {{");
+
+            int count = 0;
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    // RGB565 Convert
+                    // R: 8bit -> 5bit, G: 8bit -> 6bit, B: 8bit -> 5bit
+                    ushort rgb565 = ConvertToRGB565(pixel);
+
+                    sb.Append($"0x{rgb565:X4}"); // x16 (ex: 0xFFFF)
+
+                    if (!((x == bmp.Width - 1) && (y == bmp.Height - 1)))
+                    {
+                        sb.Append(", ");
+                    }
+
+                    count++;
+                    if (count % 16 == 0) sb.AppendLine();
+                }
+            }
+
+            sb.AppendLine("};");
+            return sb.ToString();
+        }
+        private ushort ConvertToRGB565(Color color)
+        {
+            int r = (color.R >> 3) & 0x1F;
+            int g = (color.G >> 2) & 0x3F;
+            int b = (color.B >> 3) & 0x1F;
+
+            int result = (r << 11) | (g << 5) | b;
+
+            return (ushort)result;
+        }
+    }
+}
